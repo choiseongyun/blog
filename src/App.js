ce6b136f7d7bcf9cf3f7aeb547c6f044292614b8
choiseongyun <5songyon5@naver.com>
2023-07-16 23:40:06 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let post = '강남 우동 맛집';
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
