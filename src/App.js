9d5eab94597d07be46464af6ea65fe35223c706f
choiseongyun <5songyon5@naver.com>
2023-08-01 00:30:20 +0900

    8/1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
+  let [따봉,따봉변경] = useState(0);
+  let [modal, setModal] = useState('');
+  
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 blog</div>
+        <div>ReactBlog</div>
+      </div>
+      
+      <button onClick={()=>{
+        let arraySort = [...글제목];
+        arraySort.sort();
+        console.log(arraySort);
+        글제목변경(arraySort);
+      }}>가나다순정렬</button>
+      <button onClick={()=> {
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+      <div className="list" >
+        <h4>{ 글제목[0] }
+        <span onClick={()=>{ 따봉변경( 따봉+1 )}}>👍</span> {따봉} </h4>
+        <p>2월 17일 발행 </p>
       </div>
       <div className="list">
-        <h4>글제목</h4>
+        <h4>{ 글제목[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <Modal></Modal>
     </div>
   );
 }
-
+function Modal(){
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
 export default App;
